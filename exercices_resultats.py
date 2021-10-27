8cc0c234e6b9c7ccbb80d8becfbc6c8a3e85b731
LearnCodeWithNuMa <bts.sio.cauquil.emmanuel@gmail.com>
2021-10-27 14:31:49 +0200

    Feuille d'exercice

---
+++ exercices_resultats.py
@@ -0,0 +1,187 @@
+# Exercice 1
+def pair(n):
+    """
+    >>> pair(2)
+    True
+    >>> pair(1)
+    False
+    """
+
+
+# Exercice 2
+def age(n):
+    """
+    >>> age(17)
+    'Mineur'
+    >>> age(18)
+    'Majeur'
+    """
+
+
+# Exercice 3
+def sophie():
+    """
+    >>> sophie()
+    2020 = 1000 €
+    2021 = 1120.0 €
+    2022 = 1242.4 €
+    2023 = 1367.248 €
+    2024 = 1494.5929600000002 €
+    2025 = 1624.4848192000002 €
+    2026 = 1756.9745155840003 €
+    2027 = 1892.1140058956803 €
+    2028 = 2029.9562860135939 €
+    2029 = 2170.5554117338656 €
+    2030 = 2313.966519968543 €
+    2031 = 2460.2458503679136 €
+    2032 = 2609.450767375272 €
+    2033 = 2761.6397827227775 €
+    2034 = 2916.872578377233 €
+    2035 = 3075.2100299447775 €
+    2036 = 3236.7142305436732 €
+    2037 = 3401.448515154547 €
+    2038 = 3569.4774854576376 €
+    2039 = 3740.8670351667906 €
+    2040 = 3915.6843758701266 €
+    """
+
+
+# Exercice 4
+def sophie_annee(n):
+    """
+    >>> sophie_annee(2040)
+    3915.6843758701266 €
+    """
+
+
+# Exercice 5
+def fibonacci():
+    """
+    >>> fibonacci()
+    0
+    1
+    1
+    2
+    3
+    5
+    8
+    13
+    21
+    34
+    55
+    """
+
+
+# Exercice 6
+def somme(n):
+    """
+    >>> somme(4)
+    10
+    """
+
+
+# Exercice 7
+def alea():
+    """
+    >>> alea()
+    2
+    >>> alea()
+    5
+    """
+
+
+# Exercice 8
+def nbrpair():
+    """
+    >>> pair()
+    (6, True)
+    >>> pair()
+    (5, False)
+    """
+
+
+# Exercice 9
+def guess(n):
+    """
+    >>> guess(6)
+    (6, 3, False)
+    >>> guess(3)
+    (3, 3, True)
+    """
+
+
+# Exercice 10
+def jeu():
+    """
+    >>> jeu()
+    (4, 2, 1, True)
+    >>> jeu()
+    (2, 1, 4, True)
+    >>> jeu()
+    (3, 6, 2, False)
+    """
+
+
+# Bonus
+# Exercice 10.1
+def jeu_ugo():
+    """
+    >>> jeu_ugo()
+    (4, 2, 1, True)
+    >>> jeu_ugo()
+    (2, 1, 4, True)
+    >>> jeu_ugo()
+    (3, 6, 2, False)
+    """
+
+
+# Exercice 11
+def decimal(n):
+    """
+    >>> decimal(244)
+    base 2 → 11110100
+    base 16 → f4
+    """
+
+
+# Exercice 12
+def binaire(n):
+    """
+    >>> binaire(11110100)
+    base 10 → 244
+    base 16 → f4
+    """
+
+
+# Exercice 13
+def hexadecimale(n):
+    """
+    >>> hexadecimale('f4')
+    base 10 → 244
+    base 2 → 11110100
+    """
+
+
+# Exercice 14
+def puissance(n):
+    """
+    >>> puissance(2)
+    2 ** 1 = 2
+    2 ** 2 = 4
+    2 ** 3 = 8
+    2 ** 4 = 16
+    2 ** 5 = 32
+    2 ** 6 = 64
+    2 ** 7 = 128
+    2 ** 8 = 256
+    2 ** 9 = 512
+    2 ** 10 = 1024
+    """
+
+
+# Exercice 15
+def decomposition(n):
+    """
+    >>> decomposition(2019)
+    [1024, 512, 256, 128, 64, 32, 2, 1]
+    """
